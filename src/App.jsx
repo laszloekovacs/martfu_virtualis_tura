d4a2e100ec1b835c30c76187c2334ed1b0fa71cb
laszloekovacs <laszloekovacs@gmail.com>
2022-06-09 15:34:07 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Panorama from './Panorama';
+
+function App() {
+	return (
+		<>
+			<Panorama url="assets/iroda.json"></Panorama>
+		</>
+	);
+}
+
+export default App;
